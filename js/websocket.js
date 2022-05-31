270a95ade62d3a522fe3d54bc482820887746057
Asgeir-droid <asgeir.l.aasheim@hotmail.no>
2022-05-31 10:19:50 +0200

    did some work on widgeta1 and prototype

--- js/websocket.js
+++ js/websocket.js
@@ -1,6 +1,14 @@
+const div = document.getElementById("dummy");
+const hp = document.getElementById("hp");
 const webSocket = new WebSocket("wss://co2.plug.ilderserv.no/location/bergen");
+
 webSocket.onmessage = function (event) {
     const data = event.data;
     const json = JSON.parse(data);
     console.log(json.ActivePower);
+    console.log(json.Vessels)
+    div.innerText = json.ActivePower;
+    hp.innerText = json.ActivePower * 1.34;
 }
+
+
